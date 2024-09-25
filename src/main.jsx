a23a4b2b3ecb1b5f119dbe7869b7b3e1bec189e7
cloodei <nguyenan.study@gmail.com>
2024-09-25 22:38:54 +0700

    IDK

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import App from './App.jsx'
 import './index.css'
 import { createBrowserRouter, BrowserRouter } from 'react-router-dom'
 import globalVar from './globalVar.js'
+import { Analytics } from "@vercel/analytics/react"
 
 globalVar.init();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
         <App/>
+        <Analytics></Analytics>
     </BrowserRouter>
 )
